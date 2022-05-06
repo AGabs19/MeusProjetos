7d975560c3927d027372dcc51395d8866ebbac3f
AGabs <anagabriella@infosiscs.com>
2022-05-06 08:02:41 -0300

    Migration

---
+++ VendasApp/VendasApp/DataBase/VendasAppContext.cs
@@ -0,0 +1,21 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using VendasApp.Models;
+
+namespace VendasApp.DataBase
+{
+    public class VendasAppContext : DbContext
+    {
+        public VendasAppContext (DbContextOptions<VendasAppContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Departamento> Departamento { get; set; }
+    }
+}
---
+++ VendasApp/VendasApp/Models/Departamento.cs
@@ -0,0 +1,8 @@
+﻿namespace VendasApp.Models
+{
+    public class Departamento
+    {
+        public int Id { get; set; }
+        public string Nome { get; set;}
+    }
+}
